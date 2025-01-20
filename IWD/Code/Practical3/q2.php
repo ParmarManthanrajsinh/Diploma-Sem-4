e2b29c034558eb20537acc79e69c03ebab2f164d
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2025-01-20 15:08:25 +0530

    `Refactor grade checking logic into a function and add subject marks table`

--- IWD/Code/Practical3/q2.php
+++ IWD/Code/Practical3/q2.php
@@ -1,23 +1,56 @@
 <?php
 
-$grade = 67;
-
-if ($grade >= 85) {
-    echo "Grade: AA";
-} elseif ($grade >= 75) {
-    echo "Grade: AB";
-} elseif ($grade >= 65) {
-    echo "Grade: BB";
-} elseif ($grade >= 55) {
-    echo "Grade: BC";
-} elseif ($grade >= 45) {
-    echo "Grade: CC";
-} elseif ($grade >= 40) {
-    echo "Grade: CD";
-} elseif ($grade >= 35) {
-    echo "Grade: DD";
-} else {
-    echo "Grade: FF";
+function CheckGrade($grade)
+{
+    if ($grade >= 85) {
+        return "AA";
+    } elseif ($grade >= 75) {
+        return "AB";
+    } elseif ($grade >= 65) {
+        return "BB";
+    } elseif ($grade >= 55) {
+        return "BC";
+    } elseif ($grade >= 45) {
+        return "CC";
+    } elseif ($grade >= 40) {
+        return "CD";
+    } elseif ($grade >= 35) {
+        return "DD";
+    } else {
+        return "FF";
+    }
 }
 
+$subjects = array(
+    "CN" => 67,
+    "ISE" => 98,
+    "AOOP" => 55,
+    "IWD" => 81,
+);
+
+$isPass = true;
+
+echo "<table border='1'>";
+echo "<tr><th>Subject</th><th>Marks</th><th>Grade</th></tr>";
+
+foreach ($subjects as $subject => $marks) {
+    $grade = CheckGrade($marks);
+    echo "<tr>";
+    echo "<td>$subject</td>";
+    echo "<td>$marks</td>";
+    echo "<td>$grade</td>";
+    echo "</tr>";
+
+    if ($marks < 35) {
+        $isPass = false;
+    }
+}
+
+echo "</table>";
+
+if ($isPass) {
+    echo "<h2>Result: PASS</h2>";
+} else {
+    echo "<h2>Result: FAIL</h2>";
+}
 ?>
