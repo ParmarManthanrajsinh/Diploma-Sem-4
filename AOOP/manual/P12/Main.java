38d4fbd0db88c70d7231fb35bbf436428a640f9c
ParmarManthanrajsinh <manthanrajsinh@gmail.com>
2025-01-21 15:19:20 +0530

    Added P12 and P13 in java

---
+++ AOOP/manual/P12/Main.java
@@ -0,0 +1,21 @@
+// Write a program in Java to demonstrate the 
+// constructor overloading.
+
+class Add {
+
+    Add(int x, int y) {
+        System.out.println(x + " + " + y + " = " + (x + y));
+    }
+
+    Add(int x, int y, int z) {
+        System.out.println(x + " + " + y + " + " + y + " = " + (x + y + z));
+    }
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Add add = new Add(3, 5);
+        Add add1 = new Add(3, 5, 7);
+    }
+}
